774a0e50135d41b6e877dde27b8b8ec8b042ead0
Vijet Tadasad <vijettadasad@gmail.com>
2026-04-15 10:45:35 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,4 +2,4 @@ import streamlit as st
 from groq import Groq
 st.set.page.config("PragyanAI Content  Generator",layout="wide")
 st.title("SDP_AI-content Generator")
-st.image("")
+st.image("download.jpg")
